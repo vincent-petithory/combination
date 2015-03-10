03f1b32e0422d630c3f48ece43ed3abfa3d8b363
Vincent Petithory <vincent.petithory@gmail.com>
2015-03-10 01:44:20 +0100

    Add test for multiple fields
     * use an io.Writer for writing output
     * implement multiple fields

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
 )
 
 func main() {
@@ -14,12 +14,56 @@ type FieldValues struct {
 	Values []string
 }
 
-func MakeTestTable(fieldValuesList ...FieldValues) string {
-	buf := new(bytes.Buffer)
-	for _, fv := range fieldValuesList {
-		for _, v := range fv.Values {
-			fmt.Fprintf(buf, "{%s: %s},\n", fv.Name, v)
+type Col []C
+
+type C struct {
+	Name  string
+	Value string
+}
+
+func WriteTestTable(w io.Writer, fieldValuesList ...FieldValues) error {
+	nf := len(fieldValuesList)
+
+	var cols []Col
+	for i, fv := range fieldValuesList {
+		n := 1
+		for j := i + 1; j < nf; j++ {
+			l := len(fieldValuesList[j].Values)
+			if l == 0 {
+				continue
+			}
+			n *= l
+		}
+		repeatN := 1
+		for m := 0; m < i; m++ {
+			repeatN *= 2
+		}
+
+		col := make(Col, 0, n*len(fv.Values)*repeatN)
+		for k := 0; k < repeatN; k++ {
+			for _, val := range fv.Values {
+				for j := 0; j < n; j++ {
+					col = append(col, C{Name: fv.Name, Value: val})
+				}
+			}
+		}
+		cols = append(cols, col)
+	}
+	X := len(cols)
+	Y := len(cols[0])
+
+	for y := 0; y < Y; y++ {
+		if _, err := fmt.Fprint(w, "{"); err != nil {
+			return err
+		}
+		for x := 0; x < X; x++ {
+			if _, err := fmt.Fprintf(w, "%s: %s, ", cols[x][y].Name, cols[x][y].Value); err != nil {
+				return err
+			}
+		}
+		if _, err := fmt.Fprintln(w, "},"); err != nil {
+			return err
 		}
 	}
-	return buf.String()
+	return nil
 }
--- main_test.go
+++ main_test.go
@@ -1,21 +1,59 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
-func equals(tb testing.TB, expected string, actual string) {
-	if expected != actual {
-		tb.Errorf("expected:\n%#v\ngot:\n%#v", expected, actual)
+type iotest struct {
+	Input  []FieldValues
+	Output string
+}
+
+func (test *iotest) Run(tb testing.TB) {
+	var buf bytes.Buffer
+	if err := WriteTestTable(&buf, test.Input...); err != nil {
+		tb.Fatal(err)
+	}
+	output := buf.String()
+	if test.Output != output {
+		tb.Fatalf("expected:\n%#v\ngot:\n%#v", test.Output, output)
 	}
 }
 
 func TestSingleField(t *testing.T) {
-	fieldValuesList := []FieldValues{
-		{Name: "F1", Values: []string{`"X"`, `"Y"`}},
+	test := &iotest{
+		Input: []FieldValues{
+			{Name: "F1", Values: []string{`"X"`, `"Y"`}},
+		},
+		Output: `{F1: "X", },
+{F1: "Y", },
+`,
 	}
-	expectedOutput := `{F1: "X"},
-{F1: "Y"},
-`
 
-	output := MakeTestTable(fieldValuesList...)
-	equals(t, expectedOutput, output)
+	test.Run(t)
+}
+
+func TestMultiField(t *testing.T) {
+	test := &iotest{
+		Input: []FieldValues{
+			{Name: "S1", Values: []string{`"X"`, `"Y"`}},
+			{Name: "S2", Values: []string{`"µ"`, `"v"`}},
+			{Name: "I3", Values: []string{"0xEDEA", "42", "0"}},
+		},
+		Output: `{S1: "X", S2: "µ", I3: 0xEDEA, },
+{S1: "X", S2: "µ", I3: 42, },
+{S1: "X", S2: "µ", I3: 0, },
+{S1: "X", S2: "v", I3: 0xEDEA, },
+{S1: "X", S2: "v", I3: 42, },
+{S1: "X", S2: "v", I3: 0, },
+{S1: "Y", S2: "µ", I3: 0xEDEA, },
+{S1: "Y", S2: "µ", I3: 42, },
+{S1: "Y", S2: "µ", I3: 0, },
+{S1: "Y", S2: "v", I3: 0xEDEA, },
+{S1: "Y", S2: "v", I3: 42, },
+{S1: "Y", S2: "v", I3: 0, },
+`,
+	}
+	test.Run(t)
 }
